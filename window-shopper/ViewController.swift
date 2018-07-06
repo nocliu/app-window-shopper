992bc19afdcca3698960fa210229f0c13013b9da
Connie Liu <connieliu@Lauras-MacBook-Pro-2.local>
2018-07-05 21:41:54 -0500

    first commit

---
+++ window-shopper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  window-shopper
+//
+//  Created by Connie Liu on 7/5/18.
+//  Copyright © 2018 Connie Liu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
