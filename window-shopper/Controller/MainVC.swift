ac3b26c29c63e9bea10f41a8ad410b00934976c6
Connie Liu <connieliu@Lauras-MacBook-Pro-2.local>
2018-07-05 22:33:55 -0500

    currency text field created

--- window-shopper/Controller/MainVC.swift
+++ window-shopper/Controller/MainVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainVC.swift
 //  window-shopper
 //
 //  Created by Connie Liu on 7/5/18.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
